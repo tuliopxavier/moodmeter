3f2f6fd4a92e293bedcc7b6d1e0f6bb5f922efa1
tuliopxavier <tuliopxavier@gmail.com>
2021-08-13 17:15:59 -0300

    update project with the tag input type range

--- js/script.js
+++ js/script.js
@@ -12,13 +12,19 @@ function handleEnergy() {
   } else {
     message.textContent = "Hold on tight!";
   }
+}
 
-  //   using if ternary
-  //   energy.value >= 8
-  //     ? (message.textContent = "Wow, nice!")
-  //     : energy.value <= 2
-  //     ? (message.textContent = "Hey, wait! Take a time with your cat!")
-  //     : (message.textContent = "Hold on tight!");
+function handleInput() {
+   input.value = energy.value;
+  
+  if (input.value >= 8) {
+    message.textContent = "Wow, nice!";
+  } else if (input.value <= 2) {
+    message.textContent = "Hey, wait! Take a time with your cat!";
+  } else {
+    message.textContent = "Hold on tight!";
+  }
 }
 
 input.addEventListener("change", handleEnergy);
+energy.addEventListener("change", handleInput);
